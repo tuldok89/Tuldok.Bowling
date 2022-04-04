d5339836e4253c00963c9badc5fe3f4b1ca3e5e3
Denver Ivan Gazo <tuldok89@gmail.com>
2022-04-04 21:38:33 +0800

    Enable Swagger for Prod builds

--- Tuldok.Bowling.Api/Program.cs
+++ Tuldok.Bowling.Api/Program.cs
@@ -47,12 +47,8 @@ using (var scope = app.Services.CreateScope())
     ctx.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
